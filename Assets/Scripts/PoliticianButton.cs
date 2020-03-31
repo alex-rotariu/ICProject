ee2468874743b36e73d64444e488c9795210dd67
Alex Rotariu <49029073+alex-rotariu@users.noreply.github.com>
2020-03-31 18:52:05 +0300

    Created GameMenu and Game screen scenes
    Only Assets folder and Readme.md updated

---
+++ Assets/Scripts/PoliticianButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PoliticianButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Image>().sprite = ScenesData.GetPolitician();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onClick() {
+        StartCoroutine(ShrinkButton());
+        
+    }
+
+    private IEnumerator ShrinkButton() {
+        transform.localScale = new Vector2(0.85f, 0.85f);
+        yield return new WaitForSeconds(0.02f);
+        transform.localScale = new Vector2(1f, 1f);
+    }
+}
---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Stats : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI moneyText;
+    private int money = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void addMoney() {
+        money++;
+        moneyText.text = money.ToString();
+    }
+}
