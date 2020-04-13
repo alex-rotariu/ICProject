56b9ed8e21e029e9fe01f22a1e7f24b4cee16326
Alex Rotariu <49029073+alex-rotariu@users.noreply.github.com>
2020-04-13 14:15:22 +0300

    Created side menu

---
+++ Assets/MenuSliderControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSliderControl : MonoBehaviour
+{
+    private Animator controller;
+
+    public void ShowHideMenu() {
+
+        controller = GetComponent<Animator>();
+
+        if (controller != null) {
+            bool isShowingMenu = controller.GetBool("showMenu");
+            controller.SetBool("showMenu", !isShowingMenu);
+           
+        }
+    }
+}
