2f557254719b757a6e7a24313c93993cf991d751
Alex Rotariu <49029073+alex-rotariu@users.noreply.github.com>
2020-05-02 15:30:02 +0300

    Scaling tweaks and dynamically load songs

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -1,20 +1,24 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.iOS;
 using UnityEngine;
 using UnityEngine.Audio;
 
 public class MusicPlayer : MonoBehaviour {
-    private AudioSource[] audioSources;
     private AudioSource currentSource;
     private int sourcesNumber;
+    private string[] trackNames = { "Loop Ambient008", "Loop Cinema002", "Loop Dance001", "Loop Dance004", "Loop Dreamy005", "Loop Electronic001", "Loop Pops001", "Loop Pops005", "Loop Rhythm003", "Loop Rock006"};
+    private string songPath = "LoopAndMusicFree/Loop/";
 
     // Start is called before the first frame update
     void Start() {
         DontDestroyOnLoad(this);
-        audioSources = GetComponents<AudioSource>();
-        sourcesNumber = audioSources.Length;
-        currentSource = audioSources[0];
+        sourcesNumber = trackNames.Length;
+        AudioClip audioClip = Resources.Load(songPath + trackNames[0]) as AudioClip;
+        currentSource = GetComponent<AudioSource>();
+        currentSource.clip = audioClip;
+        
     }
 
     public void SetVolume(float volume) {
@@ -28,11 +32,13 @@ public class MusicPlayer : MonoBehaviour {
             songIndex = 0;
             finish = true;
         }
+
         if (currentSource) {
             currentSource.Stop();        
         }
-        if(audioSources != null) {
-            currentSource = audioSources[songIndex];
+        if(currentSource != null) {
+            AudioClip audioClip = Resources.Load(songPath + trackNames[songIndex]) as AudioClip;
+            currentSource.clip = audioClip;
             currentSource.Play();
         }
             
