a64314b0be03d7ff9d58eab18d943440408f751c
Alex Rotariu <49029073+alex-rotariu@users.noreply.github.com>
2020-05-05 03:01:11 +0300

    Better UI for Authentification
    - nicer UI
    - field validation for input

--- Assets/Scripts/AuthController.cs
+++ Assets/Scripts/AuthController.cs
@@ -9,6 +9,7 @@ using System.Security.Cryptography;
 public class AuthController : MonoBehaviour
 {
     [SerializeField] Text emailInput, passwordInput;
+    [SerializeField] Text errorText;
 
     public void Login()
     {
@@ -32,6 +33,7 @@ public class AuthController : MonoBehaviour
 
                 if (task.IsCompleted)
                 {
+                    errorText.text = "Logged In";
                     GetComponent<SceneLoader>().loadSelectionScene();
                 }
             });
@@ -42,9 +44,21 @@ public class AuthController : MonoBehaviour
     {
         if (emailInput == null || passwordInput == null)
             return;
-        if (emailInput.text.Equals("") && passwordInput.text.Equals(""))
+        if (emailInput.text.Equals(""))
         {
-            print("Please enter an email and a password");
+            errorText.text = "Please enter a username";
+            return;
+        }
+
+        if (passwordInput.text.Equals(""))
+        {
+            errorText.text = "Please enter a password";
+            return;
+        }
+
+        if(passwordInput.text.Length > 35)
+        {
+            errorText.text = "Password too long";
             return;
         }
 
@@ -66,7 +80,10 @@ public class AuthController : MonoBehaviour
 
                 if (task.IsCompleted)
                 {
-                    print("Registration complete");
+                    emailInput.text = "";
+                    passwordInput.text = "";
+                    errorText.text = "Registration complete";
+                    
                 }
             });
     }
@@ -84,19 +101,37 @@ public class AuthController : MonoBehaviour
     void GetErrorMessage(AuthError errorCode)
     {
         string msg = "";
-        msg = errorCode.ToString();
+       
         switch (errorCode)
         {
             case AuthError.AccountExistsWithDifferentCredentials:
-                //code
+                msg = "Account with this email exists";
+                break;
+            case AuthError.EmailAlreadyInUse:
+                msg = "Email is already in use";
                 break;
             case AuthError.MissingPassword:
+                msg = "Password is missing";
                 break;
             case AuthError.WrongPassword:
+                msg = "Wrong password";
                 break;
             case AuthError.InvalidEmail:
+                msg = "Invalid email";
+                break;
+            case AuthError.WeakPassword:
+                msg = "Enter a better password";
+                break;
+            case AuthError.MissingEmail:
+                msg = "Email is missing";
+                break;
+            case AuthError.UserNotFound:
+                msg = "User not found";
+                break;
+            default:
+                msg = "An error occured";
                 break;
         }
-        print(msg);
+        errorText.text = msg;
     }
 }
